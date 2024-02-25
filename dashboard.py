c25a382b565fa9ebf177112c763d92b103d2a5fd
Xander Newlun <Xander.Newlun@my.utsa.edu>
2024-02-25 01:23:39 -0600

    Added Group Add functionality; Renamed buttons to be actual jobs

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,11 @@
 # dashboard.py
 import os
 import io
+import re
 import customtkinter as ctk
 from tkinter import *
 from tkinter import ttk
+import tkinter.messagebox as tkmb
 from google.cloud import compute_v1
 from connect_vm import connect
 credential_path = "C:\\Users\\xandy\\OneDrive\\Documents\\Rowdy24\\CyberSaurusRange\\glossy-window-415318-c67566dcade4.json"
@@ -16,13 +18,17 @@ project_id = "glossy-window-415318"
 zone = "us-west4-b"
 
 master_instance_list = []
+instance_fingerprint = ""
+compute_client = compute_v1
 
 def list_instances():
     instances_list = []
-    compute_client = compute_v1.InstancesClient()
     # Make the request to list instances
-    for instance in compute_client.list(project=project_id,zone=zone):
-        instances_list.append([instance.name,instance.status,instance.network_interfaces[0].access_configs[0].nat_i_p])
+    for instance in compute_client.InstancesClient().list(project=project_id,zone=zone):
+        global instance_fingerprint
+        instance_fingerprint = instance.label_fingerprint
+        instances_list.append([instance.name,instance.status,instance.network_interfaces[0].access_configs[0].nat_i_p,instance.labels["groups"].split("-")],)
+        print(instance.labels["groups"])
     return instances_list
 
 def deploy_instance_list(tree, search="", reload=True):
@@ -33,7 +39,6 @@ def deploy_instance_list(tree, search="", reload=True):
         master_instance_list = instances
     for instance in master_instance_list:
         if search in instance[0]:
-            connect_button = ctk.CTkButton(tree, text="Connect", command=lambda ip=instance[2]: on_connect_button_click(ip, "path/to/your/ssh/key.pem"))
             tree.insert("", "end", values=instance)
 
     tree.pack(fill='both', expand=True)
@@ -47,6 +52,47 @@ def printFocus(tree):
 
 def search_instances(event, tree):
     deploy_instance_list(tree,event.widget.get(), False)
+
+def add_group(tree, group_name="new_group"):
+    user_input = ""
+
+    is_running = True
+    while is_running:
+        input_name = ctk.CTkInputDialog(text="Enter New Group Name:", title="New Group")
+        user_input = input_name.get_input()
+        is_running = False
+        if bool(re.match(r'^[a-z0-9_-]+', user_input)) == False:
+            tkmb.showwarning(title="Error", message="Group names can only include lowercase characters, numbers, and - or _!")
+            is_running = True
+    selected = tree.selection()
+    for target in selected:
+        working_item=tree.item(target)
+        item_values = working_item['values']
+        groups = str(item_values[3]).split(" ")
+        new_groups = '-'.join(groups)
+        new_groups = new_groups + "-" + user_input
+
+
+        test = compute_client.GetInstanceRequest(
+                instance=item_values[0],
+                project=project_id,
+                zone=zone
+            )
+
+        test_res = compute_client.InstancesClient().get(request=test)
+
+
+        instance_fingerprint = test_res.label_fingerprint
+        request_body = {
+            'labels': {
+                'groups': new_groups,
+            },
+            'label_fingerprint': instance_fingerprint
+        }
+        response = compute_client.InstancesClient().set_labels(project=project_id, zone=zone, instance=item_values[0], instances_set_labels_request_resource=compute_client.InstancesSetLabelsRequest(request_body))
+        deploy_instance_list(tree, "", True)
+
+
 
 def launch_dashboard():
 
@@ -67,11 +113,11 @@ def launch_dashboard():
 
 
     # Table-like structure using Treeview
-    tree = ttk.Treeview(master=frame, columns=("VM", "Status","IP","Action"), show="headings")
+    tree = ttk.Treeview(master=frame, columns=("VM", "Status","IP", "Groups"), show="headings")
     tree.heading("VM", text="VM")
     tree.heading("Status", text="Status")
     tree.heading("IP", text="IP")
-    tree.heading("Action", text="Action")
+    tree.heading("Groups",text="Groups")
 
     deploy_instance_list(tree)
 
@@ -85,17 +131,21 @@ def launch_dashboard():
     buttons_frame = ctk.CTkFrame(master=dashboard_window)
     buttons_frame.pack(side='bottom', fill='x', padx=20, pady=10)
 
+    #Instance Buttons
+    test_button = ctk.CTkButton(master=frame, text="Open Instance", command=lambda: print("Clicked!"))
+    test_button.pack(side='left', expand=True, fill='both', padx=5, pady=5)
+
     # Define buttons
-    snapshot_btn = ctk.CTkButton(buttons_frame, text="Snapshot", command=lambda: printFocus(tree))
-    deploy_lab_btn = ctk.CTkButton(buttons_frame, text="Deploy Lab", command=lambda: deploy_instance_list(project_id, zone, tree))
-    new_vm_btn = ctk.CTkButton(buttons_frame, text="New VM", command=lambda: print("New VM clicked"))
+    snapshot_btn = ctk.CTkButton(buttons_frame, text="Connect", command=lambda: printFocus(tree))
+    deploy_lab_btn = ctk.CTkButton(buttons_frame, text="Reload VMs", command=lambda: deploy_instance_list(tree, "", True))
+    new_vm_btn = ctk.CTkButton(buttons_frame, text="Add Group", command=lambda: add_group(tree))
     destroy_btn = ctk.CTkButton(buttons_frame, text="Destroy", command=lambda: print("Destroy clicked"))
 
     # Place buttons in the buttons_frame
     snapshot_btn.pack(side='left', expand=True, fill='both', padx=5, pady=5)
     deploy_lab_btn.pack(side='left', expand=True, fill='both', padx=5, pady=5)
     new_vm_btn.pack(side='left', expand=True, fill='both', padx=5, pady=5)
-    destroy_btn.pack(side='left', expand=True, fill='both', padx=5, pady=5)
+    destroy_btn.pack(anchor='w', fill='both', padx=5, pady=5)
 
 
     dashboard_window.mainloop()
