f9a761d0937f9c284c8427541352e59d418e8e4c
perraas <80073008+perraas@users.noreply.github.com>
2024-02-24 19:51:19 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,12 +6,16 @@ from tkinter import *
 from tkinter import ttk
 from google.cloud import compute_v1
 from connect_vm import connect
-credential_path = "/home/toor/googlecreds"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
-
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("green")
 
+def setGoogle(credential_path):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+    
+def setSSH(ssh_key):
+    global key_path
+    key_path = ssh_key
+    return key_path
 def list_instances(project_id,zone):
     instances_list = []
     compute_client = compute_v1.InstancesClient()
@@ -19,11 +23,11 @@ def list_instances(project_id,zone):
     for instance in compute_client.list(project=project_id,zone=zone):
         instances_list.append([instance.name,instance.status,instance.network_interfaces[0].access_configs[0].nat_i_p])
     return instances_list
-def on_connect_button_click(ip_address, ssh_key_path):
-    connect(ip_address, ssh_key_path)
-def printFocus(tree):
+def on_connect_button_click(ip_address, key_path):
+    connect(ip_address, key_path)
+def printFocus(tree,key_path):
     curItem = tree.item(tree.focus())
-    connect(curItem["values"][2],"/home/toor/.ssh/id_rsa")
+    connect(curItem["values"][2],key_path)
 def launch_dashboard():
     project_id = "glossy-window-415318"
     zone = "us-west4-b"
@@ -56,7 +60,6 @@ def launch_dashboard():
     tree.heading("Action", text="Action")
 
     for instance in instances:
-        connect_button = ctk.CTkButton(tree, text="Connect", command=lambda ip=instance[2]: on_connect_button_click(ip, "path/to/your/ssh/key.pem"))
         tree.insert("", "end", values=instance)
 
     tree.pack(fill='both', expand=True)
@@ -67,7 +70,7 @@ def launch_dashboard():
     buttons_frame.pack(side='bottom', fill='x', padx=20, pady=10)
 
     # Define buttons
-    snapshot_btn = ctk.CTkButton(buttons_frame, text="Snapshot", command=lambda: printFocus(tree))
+    snapshot_btn = ctk.CTkButton(buttons_frame, text="Snapshot", command=lambda: printFocus(tree,key_path))
     deploy_lab_btn = ctk.CTkButton(buttons_frame, text="Deploy Lab", command=lambda: print("Deploy Lab clicked"))
     new_vm_btn = ctk.CTkButton(buttons_frame, text="New VM", command=lambda: print("New VM clicked"))
     destroy_btn = ctk.CTkButton(buttons_frame, text="Destroy", command=lambda: print("Destroy clicked"))
